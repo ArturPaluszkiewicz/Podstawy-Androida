7a1b760bf7893759b831f78114c257af8aab35f6
Axel Rechenberg <axelrechenberg@bitbucket.org>
2018-10-28 17:11:36 +0100

    Added Zadanie8.java

---
+++ src/Lista1/Zadanie8.java
@@ -0,0 +1,16 @@
+public class Zad8 {
+
+    public void search(String substring, String string){
+
+        String sub;
+        for (int index=0;index<string.length()-substring.length();index++){
+            sub = string.substring(index, index + substring.length());
+            if (sub.matches(substring)) {
+                System.out.println("jest");
+                return;
+            }
+
+        }
+        System.out.println("nie ma");
+    }
+}
